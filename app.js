e46dc9a5122da3486e1de68b375dc2553c50a5ee
welilasd <weronika.dydio@gmail.com>
2022-04-12 12:55:58 +0100

    alert upd

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const imageDate = document.querySelector('#img-date');
 const imageInfo = document.querySelector('#img-info');
 const imageAuthor = document.querySelector('#img-author');
 
+//changing date of the pic
+const dayInput = document.querySelector('#dayVal');
+const monthInput = document.querySelector('#monthVal');
+const yearInput = document.querySelector('#yearVal');
+
+const findPicture = document.querySelector('#btn-find-picture');
+
+
 //Picture information from The Astronomy Picture of the Day API
 let pictures = `https://api.nasa.gov/planetary/apod?api_key=3CI8CnF7E6wnqAFTQaXaFjs01Q1HBZJCSFqYVg42`;
 
@@ -12,9 +20,11 @@ let pictures = `https://api.nasa.gov/planetary/apod?api_key=3CI8CnF7E6wnqAFTQaXa
 //Initialise the app with data to produce a select for the characters
 initData();
 
+
 function initData(){
     fetch(pictures).then(getJson).then(updateDisplay).then(reportError);  
 }
+
 
 
 function getJson(aResponse){  //fetch response from API
@@ -22,42 +32,56 @@ function getJson(aResponse){  //fetch response from API
 }
 
 
+
 function updateDisplay(jsonObj){ 
-    console.log(jsonObj);
+    
+
+    let picObj = jsonObj;
+    console.log(picObj);
 
     //let charName = jsonObj.title;      //Get character name
-    imageTitle.textContent = jsonObj.title; //Get image title
-    imageOfTheDay.src=jsonObj.url;
-    imageAuthor.textContent = "by " + jsonObj.copyright;
-    imageDate.textContent=jsonObj.date;
-    imageInfo.textContent=jsonObj.explanation;
+    imageTitle.textContent = picObj.title; //Get image title
+
+    imageOfTheDay.src=picObj.url;
+
+    if (picObj.copyright === undefined){
+        imageAuthor.textContent = "";
+    }
+    else {
+        imageAuthor.textContent = "by " + picObj.copyright;
+    }
+
+    imageDate.textContent=picObj.date;
+    imageInfo.textContent=picObj.explanation;
+
+    //alert wrong date
+    if (picObj.code === 400){
+        //alert(picObj.msg);
+        presentAlert();
+
+        function presentAlert() {
+            const alert = document.createElement('ion-alert');
+            alert.cssClass = 'my-custom-class';
+            alert.header = 'Check the date';
+            //alert.subHeader = 'Subtitle';
+            alert.message = picObj.msg;
+            alert.buttons = ['OK'];
+          
+            document.body.appendChild(alert);
+            alert.present();
+        }
+    }
 }
 
-//-------------------------------------------------------------------
+
 
 function reportError(anError){
-    console.log(anError);
+    let error = anError;
+
+        //presentAlert(error);
+        console.log(error);
+    
 }
-
-const dayInput = document.querySelector('#dayVal');
-const monthInput = document.querySelector('#monthVal');
-const yearInput = document.querySelector('#yearVal');
-
-const outputLabel = document.querySelector('#lbl-output');
-
-const makeDate = document.querySelector('#btn-date');
-const findPicture = document.querySelector('#btn-find-picture');
-
-//makeDate.addEventListener('click', writeDate);
-
-
-
-/*function writeDate()
-{
-    theDate = yearInput.value + "-" + monthInput.value + "-"  + dayInput.value;
-    console.log(theDate);
-    outputLabel.textContent = theDate;
-}*/
 
 findPicture.addEventListener('click', changePicture);
 
@@ -67,5 +91,5 @@ function changePicture(){
 
     pictures = `https://api.nasa.gov/planetary/apod?api_key=3CI8CnF7E6wnqAFTQaXaFjs01Q1HBZJCSFqYVg42&date=` + theDate + `&concept_tags=True`;
     initData();
-    console.log(jsonObj);
 }
+
