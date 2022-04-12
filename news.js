6ec5ef0bf99024fb9793f95803e5d2580c29534d
welilasd <weronika.dydio@gmail.com>
2022-04-12 20:47:23 +0100

    news cards style upd

--- news.js
+++ news.js
@@ -7,7 +7,7 @@ const newsInfo = document.querySelector('#news-info');
 const outputList = document.getElementById("list-output");
 
 //Picture information from The Astronomy Picture of the Day API
-let news = `https://api.spaceflightnewsapi.net/v3/articles?_limit=10&title_contains=nasa`;
+let news = `https://api.spaceflightnewsapi.net/v3/articles?_limit=10&title_contains=space`;
 
 //let pictureName = "";
 
@@ -34,12 +34,28 @@ function updateDisplay(jsonObj){
         let newsObj = aNewsObj;
     //for (let i=0; i<10; i++){
         const newCardNews = document.createElement('ion-card');
+        newCardNews.setAttribute("style", "margin-bottom:5%;");
+
         const newCardHeader = document.createElement('ion-card-header');
+
         const cardTitle = document.createElement('ion-card-title');
+        cardTitle.className = "small ion-text-center"; //Assign div class
 
         const newCardContent = document.createElement('ion-card-content');
+        newCardContent.setAttribute("style", "margin-bottom:5%;");
+
         const cardImage = document.createElement('ion-img');
+        cardImage.setAttribute("style", "margin-bottom:5%;"); //Set div attributes
+
         const newsInfo = document.createElement('p');
+        newsInfo.className = "ion-text-center";
+        newsInfo.setAttribute("style", "margin-bottom:5%;");
+
+        const rightDiv = document.createElement('div');
+        rightDiv.className = "ion-text-right";
+
+        const link = document.createElement('a');
+
 
         //const cardTitle = document.createElement('ion-card-title');
 
@@ -53,11 +69,15 @@ function updateDisplay(jsonObj){
             newCardNews.appendChild(newCardContent);
             newCardContent.appendChild(cardImage);
             newCardContent.appendChild(newsInfo);
+            newCardContent.appendChild(rightDiv);
+            rightDiv.appendChild(link);
 
             cardTitle.textContent = newsObj.title;
             cardImage.src = newsObj.imageUrl;
             newsInfo.textContent = newsObj.summary;
-        //}
+            link.href = newsObj.url;
+            link.innerHTML = "Read more";
+            
     }
     console.log(newsObjArray);    
 }
