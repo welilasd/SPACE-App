6d9c4c2d98b591650fe1f1f0543cc2be0c3c09c7
welilasd <weronika.dydio@gmail.com>
2022-04-14 10:38:14 +0100

    dynamically created asteroids list

--- asteroids.js
+++ asteroids.js
@@ -3,12 +3,16 @@ const decades2 = document.querySelector('#decades-2');
 const decades3 = document.querySelector('#decades-3');
 const decades4 = document.querySelector('#decades-4');
 
+const searchButton = document.querySelector('#view-details');
+
+
 const outputList = document.querySelector('#output-list');
 
+searchButton.addEventListener('click', searchEventFired);
 
 //Picture information from The Astronomy Picture of the Day API
 //let asteroids = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY`;
-let asteroids = `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY`;
+let asteroids = `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=3CI8CnF7E6wnqAFTQaXaFjs01Q1HBZJCSFqYVg42`;
 
 //let pictureName = "";
 
@@ -26,22 +30,26 @@ function getJson(aResponse){  //fetch response from API
     return aResponse.json();
 }
 
-
+let astObj = "";
+let aAstObj = "";
+let astObjArray = "";
+let astObjDate = ""; 
 
 function updateDisplay(jsonObj){ 
-    let astObjArray = jsonObj.near_earth_objects[0].close_approach_data;
+    astObjArray = jsonObj.near_earth_objects[0].close_approach_data;
     console.log(astObjArray);
+/*
+    for (aAstObj of astObjArray){
+        astObj = aAstObj;
 
-    for (let aAstObj of astObjArray){
-        let astObj = aAstObj;
-
+        
     const newItem = document.createElement('ion-item');
     outputList.appendChild(newItem);
-    let astObjDate = astObj.close_approach_date;
+    astObjDate = astObj.close_approach_date;
 
     console.log(astObjDate);
-    newItem.textContent = astObjDate;
-}
+    newItem.textContent = astObjDate; 
+}*/
 }
 
 
@@ -52,4 +60,27 @@ function reportError(anError){
         //presentAlert(error);
         console.log(error);
     
+}
+
+
+
+
+
+function searchEventFired(){
+    console.log('lololol');
+    
+    //newItem.textContent = "lololol";
+
+    for (aAstObj of astObjArray){
+        astObj = aAstObj;
+
+        const newItem = document.createElement('ion-item');
+        newItem.setAttribute("style", "margin-left:5%;");
+        outputList.appendChild(newItem);
+    
+        astObjDate = astObj.close_approach_date;
+
+        console.log(astObjDate);
+        newItem.textContent = astObjDate;
+    }
 }
