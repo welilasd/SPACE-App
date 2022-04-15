d083ae06d9c3e3bc054150a15b1c0ba1e1cf0eab
welilasd <weronika.dydio@gmail.com>
2022-04-15 09:28:14 +0100

    list update

--- asteroids.js
+++ asteroids.js
@@ -1,17 +1,20 @@
-const decades1 = document.querySelector('#decades-1');
-const decades2 = document.querySelector('#decades-2');
-const decades3 = document.querySelector('#decades-3');
-const decades4 = document.querySelector('#decades-4');
+const searchButton1 = document.querySelector('#view-details-1');
+const searchButton2 = document.querySelector('#view-details-2');
+const searchButton3 = document.querySelector('#view-details-3');
+const searchButton4 = document.querySelector('#view-details-4');
 
-const searchButton = document.querySelector('#view-details');
+const outputList1 = document.querySelector('#output-list-1');
+const outputList2 = document.querySelector('#output-list-2');
+const outputList3 = document.querySelector('#output-list-3');
+const outputList4 = document.querySelector('#output-list-4');
 
-
-const outputList = document.querySelector('#output-list');
-
-searchButton.addEventListener('click', searchEventFired);
+searchButton1.addEventListener('click', searchEventFired1);
+searchButton2.addEventListener('click', searchEventFired2);
+searchButton3.addEventListener('click', searchEventFired3);
+searchButton4.addEventListener('click', searchEventFired4);
 
 //Picture information from The Astronomy Picture of the Day API
-//let asteroids = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY`;
+//let asteroids = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=3CI8CnF7E6wnqAFTQaXaFjs01Q1HBZJCSFqYVg42`;
 let asteroids = `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=3CI8CnF7E6wnqAFTQaXaFjs01Q1HBZJCSFqYVg42`;
 
 //let pictureName = "";
@@ -38,6 +41,7 @@ let astObjDate = "";
 function updateDisplay(jsonObj){ 
     astObjArray = jsonObj.near_earth_objects[0].close_approach_data;
     console.log(astObjArray);
+    
 /*
     for (aAstObj of astObjArray){
         astObj = aAstObj;
@@ -63,20 +67,41 @@ function reportError(anError){
 }
 
 
+function searchEventFired1(){
+    removeAllListItems();  //remove all previous list items
 
+    //astObjDate = astObj.close_approach_date;
+    /*let yyyy = "";
+    let mm = "";
+    let dd = "";
+    let date = yyyy + "-" + mm + "-" + dd;
 
+    if ((yyyy >= 1900) && (yyyy<=1950)){
+    if (astObjDate == date){*/
 
-function searchEventFired(){
-    console.log('lololol');
+        for (aAstObj of astObjArray){
+            astObj = aAstObj;
     
-    //newItem.textContent = "lololol";
+            const newItem = document.createElement('ion-item');
+            newItem.setAttribute("style", "margin-left:5%;");
+            outputList1.appendChild(newItem);
+        
+            astObjDate = astObj.close_approach_date;
+    
+            console.log(astObjDate);
+            newItem.textContent = astObjDate;
+        }
+}
+
+function searchEventFired2(){
+    removeAllListItems();  //remove all previous list items
 
     for (aAstObj of astObjArray){
         astObj = aAstObj;
 
         const newItem = document.createElement('ion-item');
         newItem.setAttribute("style", "margin-left:5%;");
-        outputList.appendChild(newItem);
+        outputList2.appendChild(newItem);
     
         astObjDate = astObj.close_approach_date;
 
@@ -84,3 +109,55 @@ function searchEventFired(){
         newItem.textContent = astObjDate;
     }
 }
+
+function searchEventFired3(){
+    removeAllListItems();  //remove all previous list items
+
+    for (aAstObj of astObjArray){
+        astObj = aAstObj;
+
+        const newItem = document.createElement('ion-item');
+        newItem.setAttribute("style", "margin-left:5%;");
+        outputList3.appendChild(newItem);
+    
+        astObjDate = astObj.close_approach_date;
+
+        console.log(astObjDate);
+        newItem.textContent = astObjDate;
+    }
+}
+
+function searchEventFired4(){
+    removeAllListItems();  //remove all previous list items
+
+    for (aAstObj of astObjArray){
+        astObj = aAstObj;
+
+        const newItem = document.createElement('ion-item');
+        newItem.setAttribute("style", "margin-left:5%;");
+        outputList4.appendChild(newItem);
+    
+        astObjDate = astObj.close_approach_date;
+
+        console.log(astObjDate);
+        newItem.textContent = astObjDate;
+    }
+}
+
+function removeAllListItems(){
+    while (outputList1.lastElementChild) {
+      outputList1.removeChild(outputList1.lastElementChild);
+    }
+    while (outputList2.lastElementChild) {
+        outputList2.removeChild(outputList2.lastElementChild);
+    }
+
+    while (outputList3.lastElementChild) {
+        outputList3.removeChild(outputList3.lastElementChild);
+    }
+
+    while (outputList4.lastElementChild) {
+        outputList4.removeChild(outputList4.lastElementChild);
+    }
+      
+  }
